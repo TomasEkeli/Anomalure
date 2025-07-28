0e4093d91d7d13c0792db7b5964e678c919bdb92
Tomas Ekeli <tomas@eke.li>
2025-07-28 10:35:55 +0000

    Add simple ping pong page with HTMX

---
+++ src/Anomalure.Web/Pages/Ping.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Anomalure.Web.Pages;
+
+public class PingModel : PageModel
+{
+    public IActionResult OnGet()
+    {
+        if (Request.Headers["HX-Request"].Count > 0)
+        {
+            return Content(
+                content: $"<strong>Pong</strong> {DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}<br/>",
+                contentType: "text/html"
+            );
+        }
+
+        return Page();
+    }
+}
