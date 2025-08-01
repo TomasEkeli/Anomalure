8daa2d838be708628d461905d7473e64bffaa3b2
Tomas Ekeli <tomas@eke.li>
2025-08-01 12:16:18 +0000

    tabs and spaces

--- src/Anomalure.Web/Pages/ServerPushes/Events.logging.cshtml.cs
+++ src/Anomalure.Web/Pages/ServerPushes/Events.logging.cshtml.cs
@@ -2,34 +2,33 @@ namespace Anomalure.Web.Pages.ServerPushes;
 
 public partial class ServerPushesEventsModel
 {
-  [LoggerMessage(
-      EventId = 1,
-      Level = LogLevel.Error,
-      Message = "Error in server push stream"
-  )]
-  static partial void LogError(
-      ILogger logger,
-      Exception ex
-  );
+	[LoggerMessage(
+		EventId = 1,
+		Level = LogLevel.Error,
+		Message = "Error in server push stream"
+	)]
+	static partial void LogError(
+		ILogger logger,
+		Exception ex
+	);
 
-  [LoggerMessage(
-      EventId = 2,
-      Level = LogLevel.Information,
-      Message = "Server push stream cancelled"
-  )]
-  static partial void LogCancelled(
-      ILogger logger,
-      TaskCanceledException ex
-  );
+	[LoggerMessage(
+		EventId = 2,
+		Level = LogLevel.Information,
+		Message = "Server push stream cancelled"
+	)]
+	static partial void LogCancelled(
+		ILogger logger,
+		TaskCanceledException ex
+	);
 
-  [LoggerMessage(
-      EventId = 3,
-      Level = LogLevel.Information,
-      Message = "Invalid operation in server push stream"
-  )]
-  static partial void LogInvalidOperation(
-      ILogger logger,
-      InvalidOperationException ex
-  );
-
+	[LoggerMessage(
+		EventId = 3,
+		Level = LogLevel.Information,
+		Message = "Invalid operation in server push stream"
+	)]
+	static partial void LogInvalidOperation(
+		ILogger logger,
+		InvalidOperationException ex
+	);
 }
